2494ca9d1282007de10621ff6c6171947efb7f8a
flexinja99 <hjgo927@naver.com>
2023-06-17 01:33:18 +0900

    6/17 키위새버튼 상점
    인벤토리 상점 버튼 수정 스크립트 3

---
+++ Kiwi/Assets/02.Scripts/Slot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    [SerializeField] Image image; //이미지 컴포넌트를 담을 곳
+
+    private Item _item;
+    public Item item
+    {
+        get { return _item; }  // item정보를 넘길때 사용
+        set
+        {
+            _item = value;
+            if (_item != null)
+            {
+                image.sprite = item.itemImage;
+                image.color = new Color(1, 1, 1, 1);
+            }
+            else
+            {
+                image.color = new Color(1, 1, 1, 0);
+            }
+        }
+    }
+}
+
