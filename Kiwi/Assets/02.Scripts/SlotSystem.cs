acf6a24340a3e8dfa568fcb6f24f65f78102dcc2
flexinja99 <hjgo927@naver.com>
2023-06-22 01:10:44 +0900

    수정 최종

--- Kiwi/Assets/02.Scripts/SlotSystem.cs
+++ Kiwi/Assets/02.Scripts/SlotSystem.cs
@@ -35,74 +35,85 @@ public class SlotSystem : MonoBehaviour
     {
         if (button == false)
         {
-            itemObject[itemNum].gameObject.active = true;
-            GameManager.UseCoin(scriptableObject[itemNum].itemCost);
-            switch(itemNum)
+            if (scriptableObject[itemNum].itemCost < GameManager.Coin)
             {
-                case 0:
-                    Debug.Log("¸Þ¶Ñ±â ¸ÀÀÖ´Ù");
-                    break;
+                itemObject[itemNum].gameObject.active = true;
+                GameManager.UseCoin(scriptableObject[itemNum].itemCost);
+                switch (itemNum)
+                {
+                    case 0:
+                        Debug.Log("¸Þ¶Ñ±â ¸ÀÀÖ´Ù");
+                        break;
 
-                case 1:
-                    Debug.Log("··ÀÌ ¸ÀÀÖ´Ù");
-                    break;
+                    case 1:
+                        Debug.Log("··ÀÌ ¸ÀÀÖ´Ù");
+                        break;
 
-                case 2:
-                    Debug.Log("½ºÆ© ¸ÀÀÖ´Ù");
-                    break;
+                    case 2:
+                        Debug.Log("½ºÆ© ¸ÀÀÖ´Ù");
+                        break;
 
-                case 3:
-                    Debug.Log("3´ë 500");
-                    break;
-                case 4:
-                    Debug.Log("ÁÖ¸ùÀÇ ÈÄ¿¹");
-                    break;
-                case 5:
-                    Debug.Log("³Ñ¾î°¡~");
-                    break;
-                case 6:
-                    Debug.Log("½Ã¿ø");
-                    break;
-                case 7:
-                    Debug.Log("¹­¾î");
-                    break;
-                case 8:
-                    Debug.Log("½»½»");
-                    break;
-                case 9:
-                    Debug.Log("»óÅ­ÇÑ ¾ÖÇÃ");
-                    break;
-                case 10:
-                    Debug.Log("µû²öÇÑ ºê·¹µå");
-                    break;
-                case 11:
-                    Debug.Log("»Í»Í");
-                    break;
-                case 12:
-                    Debug.Log("°í±Þ½º·¯¿î ÇÇ¸®");
-                    break;
-                case 13:
-                    Debug.Log("Ä¡ÀÌÀÌÀÍ");
-                    break;
-                case 14:
-                    Debug.Log("Ã·º¡Ã·º¡");
-                    break;
+                    case 3:
+                        Debug.Log("3´ë 500");
+                        break;
+                    case 4:
+                        Debug.Log("ÁÖ¸ùÀÇ ÈÄ¿¹");
+                        break;
+                    case 5:
+                        Debug.Log("³Ñ¾î°¡~");
+                        break;
+                    case 6:
+                        Debug.Log("½Ã¿ø");
+                        break;
+                    case 7:
+                        Debug.Log("¹­¾î");
+                        break;
+                    case 8:
+                        Debug.Log("½»½»");
+                        break;
+                    case 9:
+                        Debug.Log("»óÅ­ÇÑ ¾ÖÇÃ");
+                        break;
+                    case 10:
+                        Debug.Log("µû²öÇÑ ºê·¹µå");
+                        break;
+                    case 11:
+                        Debug.Log("»Í»Í");
+                        break;
+                    case 12:
+                        Debug.Log("°í±Þ½º·¯¿î ÇÇ¸®");
+                        break;
+                    case 13:
+                        Debug.Log("Ä¡ÀÌÀÌÀÍ");
+                        break;
+                    case 14:
+                        Debug.Log("Ã·º¡Ã·º¡");
+                        break;
+                    case 15:
+                        Debug.Log("¸ÀÀÖ´Â ¾¾¸®¾ó");
+                        break;
+                    case 16:
+                        Debug.Log("¸®¾ó °»½ºÅÍ");
+                        break;
 
-            }
-            if(scriptableObject[itemNum].itemType == 0)
-            {
-                GameManager.kiwi.GetFull(scriptableObject[itemNum].plusAmount);
-            }
-            else if(scriptableObject[itemNum].itemType == 1)
-            {
-                GameManager.kiwi.GetPlay(scriptableObject[itemNum].plusAmount);
-            }
-            else
-            {
-                GameManager.kiwi.GetClean(scriptableObject[itemNum].plusAmount);
+                }
+
+                if (scriptableObject[itemNum].itemType == 0)
+                {
+                    GameManager.kiwi.GetFull(scriptableObject[itemNum].plusAmount);
+                }
+                else if (scriptableObject[itemNum].itemType == 1)
+                {
+                    GameManager.kiwi.GetPlay(scriptableObject[itemNum].plusAmount);
+                }
+                else
+                {
+                    GameManager.kiwi.GetClean(scriptableObject[itemNum].plusAmount);
+                }
+                button = true;
             }
         }
-            button = true;
+
     }
 }
 
--- Kiwi/Assets/02.Scripts/UiManager.cs
+++ Kiwi/Assets/02.Scripts/UiManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UiManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    protected GameManager GameManager => GameManager.Instance;
+    public TMP_Text text;
     void Start()
     {
-        
+        text = GetComponent<TMP_Text>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        text.text = GameManager.Coin.ToString();
+
     }
 }
